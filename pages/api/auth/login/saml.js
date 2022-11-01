ea3e68512ff64747223df9546b7b8dc36ad248ab
guilherme-sudomafia <guilherme@sudomafia.com>
2022-11-01 09:28:53 -0300

    debug

--- pages/api/auth/login/saml.js
+++ pages/api/auth/login/saml.js
@@ -1,30 +1,6 @@
 import axios from "axios";
 
 export default async (req, res) => {
-  if (req.method === "POST") {
-    const { data, headers } = await axios.get("/api/auth/csrf", {
-      baseURL: "http://localhost:3000",
-    });
-    const { csrfToken } = data;
-
-    const encodedSAMLBody = encodeURIComponent(JSON.stringify(req.body));
-
-    res.setHeader("set-cookie", headers["set-cookie"] ?? "");
-    return res.send(
-      `<html>
-        <body>
-          <form action="/api/auth/callback/saml" method="POST">
-            <input type="hidden" name="csrfToken" value="${csrfToken}"/>
-            <input type="hidden" name="samlBody" value="${encodedSAMLBody}"/>
-          </form>
-          <script>
-            document.forms[0].submit();
-          </script>
-        </body>
-      </html>`
-    );
-  }
-
   res.send("here");
 
   // const createLoginRequestUrl = (identityProvider, options = {}) =>
